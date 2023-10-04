35d2e495c07c054f1894495c93ad68395e30cde9
Ahmad Hassan <ahmad.hassan.dev@pk.see.biz>
2023-10-05 03:13:44 +0500

    fix: quick fix related to accordion

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ arr.forEach((accord, index) => {
   });
 });
 
+var coll = document.getElementsByClassName("ck-collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 function hasAcceptedCookies() {
   return document.cookie.indexOf("cookie_consent=true") !== -1;
 }
